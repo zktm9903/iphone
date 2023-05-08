241a1ad4b109891c70ce5ea1cd03141740d5dcfb
zktm9903 <zktm9903@naver.com>
2023-05-08 23:42:49 +0900

    test

--- src/router.tsx
+++ src/router.tsx
@@ -1,29 +1,24 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { CameraApp, IosApp, MessageApp, MusicApp } from "./apps";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <IosApp />,
-    },
-    {
-      path: "/camera",
-      element: <CameraApp />,
-    },
-    {
-      path: "/message",
-      element: <MessageApp />,
-    },
-    {
-      path: "/music",
-      element: <MusicApp />,
-    },
-  ],
+const router = createBrowserRouter([
   {
-    basename: "/iphone",
-  }
-);
+    path: "/",
+    element: <IosApp />,
+  },
+  {
+    path: "/camera",
+    element: <CameraApp />,
+  },
+  {
+    path: "/message",
+    element: <MessageApp />,
+  },
+  {
+    path: "/music",
+    element: <MusicApp />,
+  },
+]);
 
 const Router = () => <RouterProvider router={router} />;
 
